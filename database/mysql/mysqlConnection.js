a8f86cfdc274bb48632c83ab91ecb05484a167f7
user <peter@gm99.com>
2019-06-18 11:16:01 +0800

    解决多个model 多次 connect 的问题

--- database/mysql/mysqlConnection.js
+++ database/mysql/mysqlConnection.js
@@ -1,18 +1,22 @@
 var mysql = require('mysql');
-
+var connection,connectErr = false;
 module.exports = function (config){
-	var connection = mysql.createConnection({
-		host : config.host,
-		user : config.user,
-		password : config.password,
-		database : config.databaseName
-	});
-	connection.connect(function(err) {
-        if (!err) {
-            console.log("mysql connected")
-        } else {
-            console.log("mysql connection lost");
-        }
-    });
+	if(undefined === connection || connectErr){
+		connection = mysql.createConnection({
+			host : config.host,
+			user : config.user,
+			password : config.password,
+			database : config.databaseName
+		});
+		connection.connect(function(err) {
+			if (!err) {
+				console.log("mysql connected")
+			} else {
+				console.log("mysql connection lost");
+				connectErr = true;
+			}
+		});
+	}
+	
 	return connection;
 };
